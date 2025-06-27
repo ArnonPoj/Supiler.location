786cc88741d9242560e7319cc9783fd0d2c6e4ec
ArnonPoj <bensen.f@hotmail.com>
2025-06-27 12:40:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # ตำแหน่งหมุด
     location = [13.7563, 100.5018]  # กรุงเทพ
     m = folium.Map(location=location, zoom_start=12)
     folium.Marker(location, tooltip='กรุงเทพ').add_to(m)
     m.save('templates/map.html')
-
     return render_template('map.html')
 
 if __name__ == '__main__':
