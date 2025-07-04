862f1f3c12830511b523b7daa94be02b4e1cefab
ArnonPoj <bensen.f@hotmail.com>
2025-07-04 09:29:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
 from flask import Flask, render_template
-import folium
-import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    location = [13.7563, 100.5018]
-    m = folium.Map(location=location, zoom_start=12)
-    folium.Marker(location, tooltip='กรุงเทพ').add_to(m)
-    m.save('templates/map.html')
-    return render_template('map.html')
-
-@app.route('/click-map')
-def click_map():
-    return render_template('click_map.html')
+    return render_template('map_clickable.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # 🟢 สำคัญมากสำหรับ Render
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
